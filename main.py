90dab57c76c7f901a1796b4d8b74e8abc00a939a
viskesh <vishalsaikedhari2007@gmail.com>
2026-07-06 16:33:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="Orders API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
